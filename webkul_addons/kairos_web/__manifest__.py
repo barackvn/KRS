23a5295bc3a188d4ea16fe39b18a167e80133425
khudrath@kastechssg.com <khudrath@kastechssg.com>
2021-04-15 18:37:12 +0530

    add module

---
+++ webkul_addons/kairos_web/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Kiros Website ",
+    "summary": """ """,
+    "category": "Website",
+    "version": "0.2.4",
+    "sequence": 1,
+    "author": "Planet Odoo.",
+    "depends": ['website'],
+    "data": [
+        'views/templates.xml',
+    ],
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+}
